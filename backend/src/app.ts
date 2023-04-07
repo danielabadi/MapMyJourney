25b5ebe5c8fd9d343e2eba4d1d75547f9ee2d2c5
--replace-all <igorcastejon@dcc.ufmg.br>
2023-04-07 00:16:42 -0300

    fix(users): Registro sessão

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
 import express, { Express, Router } from 'express';
+import session from 'express-session';
+import nocache from 'nocache';
 import dbConfig from '../DbConfig';
 import { DbProvider } from '../database/DbProvider';
 import { AppRouter } from './AppRouter';
+import { SESSION_CONFIG } from './users/user-access/user-session/sessions.config';
 
 export class App {
     private server: Express;
@@ -14,6 +17,8 @@ export class App {
     private setupApp(router: Router) {
         this.server.use(express.urlencoded({ extended: true }));
         this.server.use(express.json());
+        this.server.use(session(SESSION_CONFIG));
+        this.server.use(nocache());
         this.server.use(router);
     }
 
