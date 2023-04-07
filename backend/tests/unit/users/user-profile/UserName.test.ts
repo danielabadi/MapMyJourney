b31765832b624fb29cdf6c854e00129df25bb815
--replace-all <igorcastejon@dcc.ufmg.br>
2023-04-06 22:40:43 -0300

    fix(tests): Correção nome de teste

--- backend/tests/unit/users/user-profile/UserName.test.ts
+++ backend/tests/unit/users/user-profile/UserName.test.ts
@@ -1,6 +1,6 @@
 import { UserName } from '../../../../src/users/user-profile/UserName';
 
-describe('When creating a valid user name', () => {
+describe('When creating a valid user name with 5 or more characters', () => {
     it('should successfully return an user name instance', async () => {
         const userName = 'teste!!';
         expect(() => UserName.create(userName)).not.toThrow();
@@ -8,7 +8,7 @@ describe('When creating a valid user name', () => {
     });
 });
 
-describe('When creating an invalid user name', () => {
+describe('When creating an invalid user name with less than 5 characters', () => {
     it('should return an error', async () => {
         expect(() => UserName.create('abc')).toThrow();
     });
