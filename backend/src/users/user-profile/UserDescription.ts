e15f41b84b32711a4fe796170243fad691b9dc6d
--replace-all <igorcastejon@dcc.ufmg.br>
2023-04-14 20:12:30 -0300

    fix(users): Descrição vazia

--- backend/src/users/user-profile/UserDescription.ts
+++ backend/src/users/user-profile/UserDescription.ts
@@ -1,15 +1,15 @@
 export class UserDescription {
-    public readonly description: string;
+    public readonly description: string | null;
 
-    private constructor(description: string) {
+    private constructor(description: string | null) {
         this.description = description;
     }
 
-    private static isValid(description: string): boolean {
-        return description != null && description.length < 500;
+    private static isValid(description: string | null): boolean {
+        return description == null || description.length < 500;
     }
 
-    public static create(description: string): UserDescription {
+    public static create(description: string | null): UserDescription {
         if (UserDescription.isValid(description)) {
             return new UserDescription(description);
         }
--- backend/src/users/user-profile/UserProfilePersistence.ts
+++ backend/src/users/user-profile/UserProfilePersistence.ts
@@ -3,6 +3,6 @@ export type UserProfilePersistence = {
     email: string;
     name: string;
     birthdate: string;
-    description: string;
+    description: string | null;
     password: string;
 };
