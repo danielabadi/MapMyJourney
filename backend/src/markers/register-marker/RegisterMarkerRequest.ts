074bca35742011dc242b0c1e4c794c808d8095d9
--replace-all <igorcastejon@dcc.ufmg.br>
2023-04-16 13:46:27 -0300

    fix(markers): Fix request datas opcionais

--- backend/src/markers/register-marker/RegisterMarkerRequest.ts
+++ backend/src/markers/register-marker/RegisterMarkerRequest.ts
@@ -2,8 +2,8 @@ export type RegisterMarkerRequest = {
     status: string;
     title: string;
     description: string;
-    start_date: string;
-    end_date: string;
+    start_date: string | null;
+    end_date: string | null;
     lat: number;
     lng: number;
 };
