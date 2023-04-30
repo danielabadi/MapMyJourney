31c3eaacf1c3baf33482a0fee05947069d9f08d0
pdrgbr <pdrgeovanni@gmail.com>
2023-04-30 15:01:15 -0300

    feat(frontend): Criando rotas de navegação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
-import './App.css';
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<>Login</>} />
+      <Route path='/home' element={<>Home</>} />
+      <Route path='*' element={<Navigate to='/' replace />} />
+    </Routes>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { RecoilRoot } from "recoil";
+import { BrowserRouter } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "leaflet/dist/leaflet.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <CssBaseline>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CssBaseline>
+    </RecoilRoot>
+  </QueryClientProvider>
 );
