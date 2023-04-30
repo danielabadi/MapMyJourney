ce5f4e0fa93042664ef86c20a208d81b50e2e909
pdrgbr <pdrgeovanni@gmail.com>
2023-04-30 16:24:51 -0300

    feat(users): Validação do cadastro

---
+++ frontend/src/types/auth/RegisterRequestDto.ts
@@ -0,0 +1,7 @@
+export interface RegisterRequestDto {
+  name: string;
+  email: string;
+  birthdate: string;
+  password: string;
+  confirmPassword: string;
+}
---
+++ frontend/src/validations/auth/Register.schema.ts
@@ -0,0 +1,16 @@
+import * as yup from "yup";
+
+export const RegisterFormSchema = yup.object({
+  name: yup.string().required("Nome é um campo obrigatório"),
+  email: yup.string().email().required("Email é um campo obrigatório"),
+  birthdate: yup
+    .string()
+    .required("Data de nascimento é um campo obrigatório"),
+  password: yup.string().required("Senha é um campo obrigatório"),
+  confirmPassword: yup
+    .string()
+    .required("Confirmar senha é um campo obrigatório"),
+});
+
+type IRegisterForm = yup.InferType<typeof RegisterFormSchema>;
+export default IRegisterForm;
