4ff3206ee66800f1f10288fcbe0f911a620ad913
pdrgbr <pdrgeovanni@gmail.com>
2023-04-30 17:37:31 -0300

    feat(users): Estilização do menu

--- frontend/src/pages/home/components/Menu/Menu.tsx
+++ frontend/src/pages/home/components/Menu/Menu.tsx
@@ -7,6 +7,40 @@ import {
 } from "@mui/material";
 import { HiLocationMarker, HiPencilAlt } from "react-icons/hi";
 
+const typographyStyles: SxProps<Theme> = {
+  positon: "relative",
+  width: "140px",
+  marginLeft: "10px",
+  fontStyle: "normal",
+  fontWeight: "400",
+  fontSize: "16px",
+  lineHeight: "20px",
+  fontFamily: "Inter",
+  color: "#000000",
+  textAlign: "center",
+};
+
+const menuStyles: SxProps<Theme> = {
+  width: "calc(100vw*0.15)",
+  marginTop: "13px",
+
+  "& .MuiPaper-root": {
+    borderRadius: "10px",
+    maxWidth: "300px",
+  },
+
+  "& .MuiList-root": {
+    paddingTop: "0px",
+    paddingBottom: "0px",
+    height: "80px",
+  },
+
+  "& .MuiMenuItem-root": {
+    paddingY: "3px",
+    height: "40px",
+  },
+};
+
 export interface MenuProps {
   anchorElement: null | HTMLElement;
   open: boolean;
@@ -24,17 +58,18 @@ const Menu: React.FC<MenuProps> = ({ anchorElement, open, onClose }) => {
 
   return (
     <MuiMenu
+      sx={menuStyles}
       anchorEl={anchorElement}
       open={open}
       onClose={onClose}
     >
       <MenuItem disableRipple onClick={handleEditarPerfil}>
         <HiPencilAlt size={"20px"} />
-        <Typography>Editar Perfil</Typography>
+        <Typography sx={typographyStyles}>Editar Perfil</Typography>
       </MenuItem>
       <MenuItem disableRipple onClick={handleList}>
         <HiLocationMarker size={"20px"} />
-        <Typography >Meus Marcadores</Typography>
+        <Typography sx={typographyStyles}>Meus Marcadores</Typography>
       </MenuItem>
     </MuiMenu>
   );
