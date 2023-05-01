93841abd9f340e94c58a086f2a08530f1a26167c
Daniel Abadi <48056794+danielabadi@users.noreply.github.com>
2023-05-01 11:39:27 -0300

    feat(marcadores): Adicionando dados dos detalhes dos marcadores

--- frontend/src/pages/map/components/DetailsMarker/DetailsMarker.js
+++ frontend/src/pages/map/components/DetailsMarker/DetailsMarker.js
@@ -1,9 +1,57 @@
 import React from "react";
 import "./DetailsMarker.css";
+import { MdClose } from "react-icons/md";
+import { useSetRecoilState, useRecoilValue } from "recoil";
+import { showDetailsMarkerState } from "../../../../states/detailsmarker/atom";
+import { markersState } from "../../../../states/markers/atom";
+import moment from 'moment'
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function DetailsMarker(props) {
+    const setShowDetailsMarker = useSetRecoilState(showDetailsMarkerState);
+    const markers = useRecoilValue(markersState);
+    const indexMarker = markers.findIndex(m => m.id === props.marker);
+    let marker = markers[indexMarker];
+
+    React.useEffect(() => {
+        marker = markers[indexMarker];
+    }, [markers]);
+
+    function getColor(status) {
+        return status === 'ja fui' ? '#073064' :
+            status === 'quero ir' ? 'green' : '#dede09';
+    }
+
+    function handleClick() {
+        setShowDetailsMarker(false);
+    }
+
     return (
-        <div className="details_marker"></div>
+        <div className="details_marker">
+            <div className="details_marker_content">
+                <MdClose size={'30px'} color="#808080" className="details_marker_close" onClick={() => handleClick()}></MdClose>
+                <div className="details_marker_elements">
+                    <h1>{marker.title}</h1>
+                    {marker.photos.length > 2 &&
+                        <Carousel showArrows={true} className="Carousel">
+                            {marker.photos.map(element => (
+                                <div key={element}>
+                                    <img src={'http://localhost:8001/uploads/' + element} alt="imagem viagem" />
+                                </div>
+                            ))}
+                        </Carousel>}
+                    {marker.photos.length === 1 &&
+                        <img style={{ width: "100%" }} src={'http://localhost:8001/uploads/' + marker.photos[0]} alt="imagem viagem" />
+                    }
+                    <div className="details_marker__date">
+                        <span className="dot" style={{ backgroundColor: getColor(marker.status) }}></span>
+                        <p>{marker.start_date !== null ? moment(marker.start_date).utc().format('DD/MM/YYYY') : 'Sem data de ida'} - {marker.end_date !== null ? moment(marker.end_date).utc().format('DD/MM/YYYY') : 'Sem data de volta'}</p>
+                    </div>
+                    <p className="details_marker__description">{marker.description}</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
