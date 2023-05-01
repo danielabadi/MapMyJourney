4122c7dd9dea0cb4eb8770866c809a27b3b465be
Daniel Abadi <48056794+danielabadi@users.noreply.github.com>
2023-05-01 11:56:35 -0300

    feat(marcadores): Adição da lógica de mostrar tela de edição de marcadores

--- frontend/src/pages/map/components/DetailsMarker/DetailsMarker.js
+++ frontend/src/pages/map/components/DetailsMarker/DetailsMarker.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./DetailsMarker.css";
 import { MdClose } from "react-icons/md";
-import { useSetRecoilState, useRecoilValue } from "recoil";
+import { useSetRecoilState, useRecoilValue, useRecoilState } from "recoil";
 import { showDetailsMarkerState } from "../../../../states/detailsmarker/atom";
 import { markersState } from "../../../../states/markers/atom";
 import moment from 'moment'
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { showEditMarkerState } from "../../../../states/editmarker/atom";
+import { MdModeEdit } from "react-icons/md";
+import EditMarker from "../EditMarker/EditMarker";
 
 function DetailsMarker(props) {
     const setShowDetailsMarker = useSetRecoilState(showDetailsMarkerState);
+    const [showEditMarker, setShowEditMarker] = useRecoilState(showEditMarkerState);
     const markers = useRecoilValue(markersState);
     const indexMarker = markers.findIndex(m => m.id === props.marker);
     let marker = markers[indexMarker];
@@ -27,30 +31,40 @@ function DetailsMarker(props) {
         setShowDetailsMarker(false);
     }
 
+    function handleEdit() {
+        setShowEditMarker(true);
+    }
+    
     return (
         <div className="details_marker">
-            <div className="details_marker_content">
-                <MdClose size={'30px'} color="#808080" className="details_marker_close" onClick={() => handleClick()}></MdClose>
-                <div className="details_marker_elements">
-                    <h1>{marker.title}</h1>
-                    {marker.photos.length > 2 &&
-                        <Carousel showArrows={true} className="Carousel">
-                            {marker.photos.map(element => (
-                                <div key={element}>
-                                    <img src={'http://localhost:8001/uploads/' + element} alt="imagem viagem" />
-                                </div>
-                            ))}
-                        </Carousel>}
-                    {marker.photos.length === 1 &&
-                        <img style={{ width: "100%" }} src={'http://localhost:8001/uploads/' + marker.photos[0]} alt="imagem viagem" />
-                    }
-                    <div className="details_marker__date">
-                        <span className="dot" style={{ backgroundColor: getColor(marker.status) }}></span>
-                        <p>{marker.start_date !== null ? moment(marker.start_date).utc().format('DD/MM/YYYY') : 'Sem data de ida'} - {marker.end_date !== null ? moment(marker.end_date).utc().format('DD/MM/YYYY') : 'Sem data de volta'}</p>
+            {
+                !showEditMarker ?
+                    <div className="details_marker_content">
+                        <MdClose size={'30px'} color="#808080" className="details_marker_close" onClick={() => handleClick()}></MdClose>
+                        <div className="details_marker_elements">
+                            <h1>{marker.title}</h1>
+                            {marker.photos.length > 2 &&
+                                <Carousel showArrows={true} className="Carousel">
+                                    {marker.photos.map(element => (
+                                        <div key={element}>
+                                            <img src={'http://localhost:8001/uploads/' + element} alt="imagem viagem" />
+                                        </div>
+                                    ))}
+                                </Carousel>}
+                            {marker.photos.length === 1 &&
+                                <img style={{ width: "100%" }} src={'http://localhost:8001/uploads/' + marker.photos[0]} alt="imagem viagem" />
+                            }
+                            <div className="details_marker__date">
+                                <span className="dot" style={{ backgroundColor: getColor(marker.status) }}></span>
+                                <p>{marker.start_date !== null ? moment(marker.start_date).utc().format('DD/MM/YYYY') : 'Sem data de ida'} - {marker.end_date !== null ? moment(marker.end_date).utc().format('DD/MM/YYYY') : 'Sem data de volta'}</p>
+                            </div>
+                            <p className="details_marker__description">{marker.description}</p>
+                        </div>
+                        <button className="edit_button" onClick={handleEdit}><MdModeEdit size={'14px'} className="icon_edit"></MdModeEdit> Editar</button>
                     </div>
-                    <p className="details_marker__description">{marker.description}</p>
-                </div>
-            </div>
+                    :
+                    marker !== undefined && <EditMarker marker={marker}></EditMarker>
+            }
         </div>
     )
 }
---
+++ frontend/src/pages/map/components/EditMarker/EditMarker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './EditMarker.css';
+
+function EditMarker(props) {
+    return (
+        <div className='edit-form'></div>
+    )
+}
+
+export default EditMarker;
