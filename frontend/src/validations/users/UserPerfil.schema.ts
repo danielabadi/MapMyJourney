fabbb723453f54cf90da12c748cb42856f7284cc
pdrgbr <pdrgeovanni@gmail.com>
2023-05-21 13:26:35 -0300

    fix(users): Arrumando validações da edição de perfil

--- frontend/src/validations/users/UserPerfil.schema.ts
+++ frontend/src/validations/users/UserPerfil.schema.ts
@@ -1,15 +1,20 @@
 import * as yup from "yup";
 
 export const userPerfilFormSchema = yup.object({
-  name: yup.string().required("Nome é um campo obrigatório"),
+  name: yup.string().min(5, 'Nome deve ter no mínimo 5 caracteres').required("Nome é um campo obrigatório"),
   email: yup.string().email().required("Email é um campo obrigatório"),
   birthdate: yup
     .string()
     .required("Data de nascimento é um campo obrigatório"),
   description: yup.string(),
-  password: yup.string().required("Senha é um campo obrigatório"),
-  newPassword: yup.string(),
-  confirmPassword: yup.string(),
+  password: yup.string().min(5, 'Senha deve ter no mínimo 5 caracteres').required("Senha é um campo obrigatório"),
+  newPassword: yup.string().min(5, 'Senha deve ter no mínimo 5 caracteres'),
+  confirmPassword: yup
+  .string()
+  .test('passwords-match', 'Senhas devem ser iguais', 
+    function (value) {       
+      return this.parent.newPassword === value     
+    }),
 });
 
 type IUserPerfilForm = yup.InferType<typeof userPerfilFormSchema>;
