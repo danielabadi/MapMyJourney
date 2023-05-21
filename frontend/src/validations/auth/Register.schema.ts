e78b3577d78934912e0ec0e96cef10daa041f5bd
pdrgbr <pdrgeovanni@gmail.com>
2023-05-21 13:22:05 -0300

    fix(users): Arrumando as validações para o registro de novos usuários

--- frontend/src/validations/auth/Register.schema.ts
+++ frontend/src/validations/auth/Register.schema.ts
@@ -1,15 +1,19 @@
 import * as yup from "yup";
 
 export const registerFormSchema = yup.object({
-  name: yup.string().required("Nome é um campo obrigatório"),
-  email: yup.string().email().required("Email é um campo obrigatório"),
+  name: yup.string().min(5, 'Nome deve ter no mínimo 5 caracteres').required("Nome é um campo obrigatório"),
+  email: yup.string().email('Insira um email válido').required("Email é um campo obrigatório"),
   birthdate: yup
     .string()
     .required("Data de nascimento é um campo obrigatório"),
-  password: yup.string().required("Senha é um campo obrigatório"),
+  password: yup.string().min(5, 'Senha deve ter no mínimo 5 caracteres').required("Senha é um campo obrigatório"),
   confirmPassword: yup
     .string()
-    .required("Confirmar senha é um campo obrigatório"),
+    .required("Confirmar senha é um campo obrigatório")
+    .test('passwords-match', 'Senhas devem ser iguais', 
+    function (value) {       
+      return this.parent.password === value     
+    }),
 });
 
 type IRegisterForm = yup.InferType<typeof registerFormSchema>;
